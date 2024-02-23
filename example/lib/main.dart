2fb2bcd358a394abd2016960f984373fff7f7f36
Felix Nyabwari <felix.nyabwari@ogilvy.africa>
2024-02-23 11:55:23 +0300

    Updates example page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Edge Alert',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Smart Edge Alert'),
     );
   }
 }
