fdeca1f51e34007709c0d69278f3c26010461e28
Felix Ogucha <fenicfelix@gmail.com>
2025-08-05 13:33:03 +0100

    Updates example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,9 +29,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+      appBar: AppBar(title: Text(title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -53,58 +51,66 @@ class MyHomePage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 // Navigate to the second screen using a named route.
-                SmartEdgeAlert.show(context,
-                    title: 'Title',
-                    description: 'Description',
-                    gravity: SmartEdgeAlert.bottom,
-                    closeButtonColor: Colors.white,
-                    duration: SmartEdgeAlert.lengthVeryLong);
+                SmartEdgeAlert.show(
+                  context,
+                  title: 'Title',
+                  description: 'Description',
+                  gravity: SmartEdgeAlert.bottom,
+                  closeButtonColor: Colors.white,
+                  duration: SmartEdgeAlert.lengthVeryLong,
+                );
               },
               child: const Text('Alert From Bottom'),
             ),
             ElevatedButton(
               onPressed: () {
                 // Navigate to the second screen using a named route.
-                SmartEdgeAlert.show(context,
-                    title: 'SUCCESS',
-                    description: 'Description',
-                    icon: Icons.check,
-                    backgroundColor: Colors.green,
-                    gravity: SmartEdgeAlert.top,
-                    closeButtonColor: Colors.white,
-                    duration: SmartEdgeAlert.lengthVeryLong);
+                SmartEdgeAlert.show(
+                  context,
+                  title: 'SUCCESS',
+                  description: 'Description',
+                  icon: Icons.check,
+                  backgroundColor: Colors.green,
+                  gravity: SmartEdgeAlert.top,
+                  closeButtonColor: Colors.white,
+                  duration: SmartEdgeAlert.lengthVeryLong,
+                );
               },
               child: const Text('Colorful Alert'),
             ),
             ElevatedButton(
               onPressed: () {
                 // Navigate to the second screen using a named route.
-                SmartEdgeAlert.show(context,
-                    title: 'Title',
-                    description:
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
-                    backgroundColor: Colors.red,
-                    icon: Icons.error,
-                    gravity: SmartEdgeAlert.bottom,
-                    closeButtonColor: Colors.white,
-                    duration: SmartEdgeAlert.lengthVeryLong);
+                SmartEdgeAlert.show(
+                  context,
+                  title: 'Title',
+                  description:
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
+                  backgroundColor: Colors.red,
+                  icon: Icons.error,
+                  gravity: SmartEdgeAlert.bottom,
+                  closeButtonColor: Colors.white,
+                  duration: SmartEdgeAlert.lengthVeryLong,
+                );
               },
               child: const Text('Long Description Alert'),
             ),
             ElevatedButton(
               onPressed: () {
                 // Navigate to the second screen using a named route.
-                SmartEdgeAlert.show(context,
-                    title: 'Title',
-                    titleFontSize: 30,
-                    description:
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
-                    descriptionFontSize: 20,
-                    backgroundColor: Colors.orange,
-                    gravity: SmartEdgeAlert.bottom,
-                    closeButtonColor: Colors.white,
-                    icon: Icons.warning,
-                    duration: SmartEdgeAlert.lengthVeryLong);
+                SmartEdgeAlert.show(
+                  context,
+                  title: 'Title',
+                  titleFontSize: 30,
+                  description:
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
+                  descriptionFontSize: 20,
+                  backgroundColor: Colors.orange,
+                  gravity: SmartEdgeAlert.bottom,
+                  closeButtonColor: Colors.white,
+                  icon: Icons.warning,
+                  duration: SmartEdgeAlert.lengthVeryLong,
+                );
               },
               child: const Text('Custom Font Size Alert'),
             ),
